5ea7a0aa69d83050d018b3221baaa4b8c24f9711
이완호 <dldhksgh90@futurewiz.co.kr>
2019-02-19 17:51:06 +0900

    인코딩 utf-8 적용
    encoding - utf-8

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,7 +1,22 @@
 package main;
 
+import java.lang.reflect.Field;
+import java.nio.charset.Charset;
+
 public class Main {
 	public static void main(String args[]) {
+		System.setProperty("file.encoding", "utf-8");
+		try {
+			Field charset = Charset.class.getDeclaredField("defaultCharset");
+			charset.setAccessible(true);
+			charset.set(null,null);
+		} catch (NoSuchFieldException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		new MainFrame();
 	}
 }
